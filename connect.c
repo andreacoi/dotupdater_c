4d7cfa9df654f2c9992c50c46dd71eea0ede9d44
Andrea Coi <andrea.coi@outlook.it>
2025-12-24 20:04:32 +0100

    writes a function to verify if the device is connected to the internet

---
+++ connect.c
@@ -0,0 +1,27 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+// function that check if the device is connected
+// return 0 if the device is connected, 1 otherwise.
+int is_connected() {
+  // declare the struct required by the manpage.
+  struct addrinfo hints, *res;
+  // init the status
+  int status = 0;
+
+  // initialize to zero the hints struct  
+  memset(&hints, 0, sizeof(hints));
+  
+  // specify if the check has to be done in IPv4 or IPv6 
+  // - if unspec, not important.
+  hints.ai_family = AF_UNSPEC;
+  //  specify the sock type
+  hints.ai_socktype = SOCK_STREAM;  
+
+  // call to getaddrinfo to check the connection
+  status = getaddrinfo("8.8.8.8", "80", &hints, &res);
+  // freeing the linked list
+  freeaddrinfo(res); 
+  return status;
+}

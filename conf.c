fa3c2eaebe6b12a352a1f191ec51f9a85a1f6da8
Andrea Coi <andrea.coi@outlook.it>
2025-12-27 18:56:34 +0100

    uses and tests the function to sum the strings

--- conf.c
+++ conf.c
@@ -1,68 +1,53 @@
 #include <stdio.h>
 #include "conf.h"
+#include "helper.h"
 #include <sys/stat.h>
 #include <stdlib.h>
 #include <string.h>
-
-// function to retrieve the home dir using env $HOME
 
 char *getHomeDir() {
   char *homedir = getenv("HOME");
   return homedir;  
 }
 
-// function to retrieve the config folder using the concatenation of the 
-// string CONFIGURATION_PATH and the return value of the function getHomeDir().
-
 char *getConfigFolder() {
-  char *fp = CONFIGURATION_PATH;
-  char *hd = getHomeDir();
-
-  size_t lfp = strlen(fp);
-  size_t lhd = strlen(hd);
-  char *cf = malloc(lfp + lhd + 1);
-  cf = strcpy(cf, hd);
-  cf = strcat(hd, fp);
+  char *dir = CONFIGURATION_PATH;
+  char *homedir = getHomeDir();
+  char *cf = sumStrings(homedir, dir);
   return cf;
 }
 
-// wrapper function to create the config folder, it returns same value of 
-// mkdir(3).
+char *getConfigFile() {
+  // create a char* and put in it the value of the constant that contains the 
+  // name of the configuration file.
+  char *f = CONFIGURATION_FILE;
+  char *cf = getConfigFolder();
+  char *complete = sumStrings(cf, f);
+  return complete;
+}
+
+
 
 int createConfigFolder(char *cf) {
   // TODO: insert the logger functions
   return mkdir(cf, CONF_PATH_MODE); 
 }
 
-int createBluePrintConf(char *cf) {
-  // check if the folder exists
-  if (checkConfDir(cf) == 0) {
-    // if the folder exists, check if the file exists
-  }
+int createBluePrintConf(char *cf, char *f) {
+  char *f = sumStrings(cf, f);
+  char buf[PLACEHOLDER_TEXT_SIZE] = "[[repositories]]\npath = \"yourpath\"\nbranch = \"yourbranchongh\"\n# this is the structure of the file, replicate it to manage more repos.";
+  FILE *bpf = fopen(f, "w");
+  fwrite(buf,PLACEHOLDER_TEXT_SIZE, 1, bpf);
+  free(f);
+  fclose(bpf);
+  return 1;
+}
+
+
+int isConfDirExists(char *cf) {
   return 0;
 }
 
-// function to check if the config folder already exists, includes a recursive
-// call to createConfigFolder. Returns 0 if the folder exists or the return
-// value of the createConfigFolder().
-
-int checkConfDir(char *cf) {
-  struct stat st;
-  if (stat(cf, &st) == 0 && S_ISDIR(st.st_mode)) {
-  // TODO: insert the logger functions
-    return 0;
-  } else {
-    return createConfigFolder(cf);
-  }
-}
-
-int checkConfFile(char *cf) {
-  char *f = CONFIGURATION_FILE;
-  size_t lfp = strlen(cf);
-  size_t lf = strlen(f);
-  char *cp = malloc(lfp + lf + 1);
-  cp = strcpy(cp, cf);
-  cp = strcat(cf, f);
-  printf("%s", cp);
+int isConfFileExists(char *cf, char *f) {
   return 0;
 } 

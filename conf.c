9efe4d21a7f421f55114314cdd13c29866c280f6
Andrea Coi <andrea.coi@outlook.it>
2025-12-28 15:46:01 +0100

    adds some security controls in order to prevent some dangerous memleaks

--- conf.c
+++ conf.c
@@ -21,12 +21,8 @@ char *getConfigFile() {
   // create a char* and put in it the value of the constant that contains the 
   // name of the configuration file.
   char *f = CONFIGURATION_FILE;
-  char *cf = getConfigFolder();
-  char *complete = sumStrings(cf, f);
-  return complete;
+  return f;
 }
-
-
 
 int createConfigFolder(char *cf) {
   // TODO: insert the logger functions
@@ -34,20 +30,55 @@ int createConfigFolder(char *cf) {
 }
 
 int createBluePrintConf(char *cf, char *f) {
-  char *f = sumStrings(cf, f);
+  char *filepath = sumStrings(cf, f);
+  if (filepath == NULL) return -1;
+  printf(filepath);
   char buf[PLACEHOLDER_TEXT_SIZE] = "[[repositories]]\npath = \"yourpath\"\nbranch = \"yourbranchongh\"\n# this is the structure of the file, replicate it to manage more repos.";
-  FILE *bpf = fopen(f, "w");
-  fwrite(buf,PLACEHOLDER_TEXT_SIZE, 1, bpf);
-  free(f);
+  FILE *bpf = fopen(filepath, "w+");
+  if (bpf == NULL) {
+    free(filepath);
+    return -1;
+  }
+  if (fwrite(buf,sizeof(char),strlen(buf), bpf) < strlen(buf)) {
+    perror("write error");
+    return -1;
+  }
+  free(filepath);
   fclose(bpf);
-  return 1;
+  return 0;
 }
 
-
 int isConfDirExists(char *cf) {
-  return 0;
+  struct stat st;
+  if (stat(cf, &st) && S_ISDIR(st.st_mode)) {
+    return 0;
+  } else {
+    return 1;
+  }
 }
 
 int isConfFileExists(char *cf, char *f) {
+  struct stat st;
+  char *path = sumStrings(cf, f);
+  if (stat(path, &st) && S_ISREG(st.st_mode)) {
+    free(path);
+    return 0;
+  } else {
+    free(path);
+    return 1;
+  }
+}
+
+int preliminaryCheck(char *cf, char *f) {
+  if (isConfDirExists(cf) == 0 && isConfFileExists(cf, f) == 0) {
+    return 0;
+  } else if (isConfDirExists(cf) == 1) {
+    createConfigFolder(cf);
+    createBluePrintConf(cf, f);
+    return -2;
+  } else if (isConfFileExists(cf, f) == 1) {
+    createBluePrintConf(cf, f);
+    return -1;
+  }
   return 0;
-} 
+}

0bce95a46a1e4d46d55017d5dea86581c59c4885
Andrea Coi <andrea.coi@outlook.it>
2025-12-31 19:10:24 +0100

    helper to declare concatStrings function

--- helper.h
+++ helper.h
@@ -1,8 +1,10 @@
+#define concatStrings(...) cStr(__VA_ARGS__, NULL)
+
 /**
- * @brief function to concatenate 2 dynamic strings, the concatenated strings starts with s1 and ends with s2, followed by \0.
- *        The freeing of memory it's up to the caller.
+ * @brief variadic function to concatenate n dynamic strings
+ * Freeing memory it's up to the caller.
  * @params char *s1, the first string
  *         char *s2, the second string
  * @return char * -> the string resulting of the concatenation process.
 */
-char *sumStrings(char *s1, char *s2);
+char *cStr(char *first, ...);

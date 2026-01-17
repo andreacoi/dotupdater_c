f0c6c89abf3c4dc86ea06577d45d3ce7d2452534
Andrea Coi <andrea.coi@outlook.it>
2026-01-17 17:14:32 +0100

    writes the function to log
    TODO: fix for some memleaks (valgrind says 29), consequentially SIGSEGV,
    write the error string to the log file specified in the logger.h

--- logger.c
+++ logger.c
@@ -1,14 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
 #include "logger.h"
+#include "helper.h"
 
-void log(char *message, int type, char *path) {
-  if (stat(path, &st) && S_ISREG(st.st_mode)) {
-    Message *m;
-    m->type = type;
-    m->text = "test";
-    m->date = "prova";
-  } else {
-    perror("log file/folder not found");
-    return -1;
-  }
+void dup_log(char *message, int type) {
+    Message *m = malloc((sizeof(message) *3) + 1);
+    m->text = message;
+    m->date = getCurrentDate();
+    switch (type) {
+      case 1:
+        m->type = " [E] ";
+        break;
+      case 2:
+        m->type = " [OSINT] ";
+        break;
+      case 3:
+        m->type = " [CERR] ";
+        break;
+      case -1:
+        m->type = " [INFO] ";
+        break;
+      case -2:
+        m->type = " [W] ";
+        break;
+    }
+    char *estr = cStr("[", m->date, "] - ", m->type, " - ", m->text, "\n");
+    printf("%s", estr);
+    free(estr);
 }

33581fdb6b4f441a63deea1458a89b4f1ad8cce0
Andrea Coi <andrea.coi@outlook.it>
2026-01-18 12:33:43 +0100

    fixes SIGSEV ref @f0c6c89a

--- logger.c
+++ logger.c
@@ -5,27 +5,36 @@
 #include "helper.h"
 
 void dup_log(char *message, int type) {
-    Message *m = malloc((sizeof(message) *3) + 1);
+    Message *m = malloc(sizeof(Message));
+    if (m == NULL) return;
     m->text = message;
     m->date = getCurrentDate();
+    // cleanup if malloc for the date fails for some reason.
+    if (m->date == NULL) return;
     switch (type) {
       case 1:
-        m->type = " [E] ";
+        m->type = "[E]";
         break;
       case 2:
-        m->type = " [OSINT] ";
+        m->type = "[OSINT]";
         break;
       case 3:
-        m->type = " [CERR] ";
+        m->type = "[CERR]";
         break;
       case -1:
-        m->type = " [INFO] ";
+        m->type = "[INFO]";
         break;
       case -2:
-        m->type = " [W] ";
+        m->type = "[W]";
+        break;
+      default:
+        m->type = "[LOG]";
         break;
     }
-    char *estr = cStr("[", m->date, "] - ", m->type, " - ", m->text, "\n");
+    char *estr = cStr("[", m->date, "] - ", m->type, " - ", m->text, "\n", NULL);
     printf("%s", estr);
+    // free after using estr
     free(estr);
+    // free m->date after using it.
+    free(m->date);
 }
